d289f92fb7c39f1ce21561de6bb8ae2f523f4477
yangxu <273423480@qq.com>
2020-04-16 17:50:06 +0800

    修改 配制

--- packages/fxTool/index.js
+++ packages/fxTool/index.js
@@ -2,6 +2,6 @@ import BigNumber from 'bignumber.js'
 
 // 大数单位缩小n位
 export function unitSmaller (val, n) {
-  let unitUnm = n || 0
+  let unitUnm = n || 1
   return new BigNumber(val).multipliedBy(new BigNumber('1e' + unitUnm)).toFixed()
 }
